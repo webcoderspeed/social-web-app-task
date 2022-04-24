25c3e5ceb878bac410ea7244f443e202090bc749
webcoderspeed <webcoderspeed@gmail.com>
2022-04-24 21:29:36 +0530

    package.json updated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,7 +6,7 @@ const BASE_URL = process.env.BASE_URL ?? '/api/v1';
 
 const routes = (app) => {
   // setting up the routes
-  app.get((req, res) => res.send('Server is running'));
+  app.use((req, res) => res.send('Server is running'));
 
   // user routes
   app.use(`${BASE_URL}/users`, userRoutes);
